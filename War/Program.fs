d55084dbe0432e4afe684cc27266fd551cc39400
Matas <m.bartaska@gmail.com>
2018-02-07 23:06:59 +0200

    Removed exception in constructor. Optimised sort logic

--- War/Program.fs
+++ War/Program.fs
@@ -10,13 +10,13 @@ type CardSuit=
 
 
 type Card(suit:CardSuit,value:int)=
-    do
-        if value>12 then
-            invalidArg "value" "Invalid card value"
+    //do
+    //    if value>12 then
+    //        invalidArg "value" "Invalid card value"
     
     member this.Suit=suit
-    member this.Value=value
-    override this.ToString()= sprintf "{Suit: %A; Value: %i;}" this.Suit this.Value
+    member this.Value=value%13
+    override this.ToString()= sprintf "{Suit: %A; Value: %i;}" this.Suit (this.Value+2)
 
     
 let generateCard x= 
@@ -52,18 +52,32 @@ let compareByValue (first: Card) (second: Card) (trump: CardSuit)=
             false 
 
 let formPointStack (first:Card list) (second:Card list) (trump:CardSuit)=
+        let bothPiles=second
+                        |>List.zip first 
+                        |>List.choose(fun (x,y) ->
+                            if(x.Suit=trump&&y.Suit<>trump||compareByValue x y trump) 
+                            then Some [(0,x);(0,y)]
+                            elif((x.Suit<>trump&&y.Suit<>trump||x.Suit=trump&&y.Suit=trump)&&x.Value=y.Value) 
+                            then Some [(0,x);(1,y)] 
+                            else
+                            Some[(1,x);(1,y)]
+                        )
+                        |>List.concat
+                        |>List.groupBy(fun (x,y)->x)
+                        
+        let firstPlayerPile=bothPiles
+                            |>List.filter(fun (x,y)->x=0)
+                            |>List.map(fun (x,y)->y)
+                            |>List.concat
+                            |>List.map(fun (x,y)->y)
 
-        second 
-        |>List.zip first 
-        |>List.choose(fun (x,y) ->
-          if(x.Suit=trump&&y.Suit<>trump||compareByValue x y trump) 
-          then Some [x;y]
-          elif((x.Suit<>trump&&y.Suit<>trump||x.Suit=trump&&y.Suit=trump)&&x.Value=y.Value) 
-          then Some [x] 
-          else
-            None
-        )
-        |>List.concat
+        let secondPlayerPile=bothPiles
+                            |>List.filter(fun (x,y)->x=1)
+                            |>List.map(fun (x,y)->y)       
+                            |>List.concat
+                            |>List.map(fun (x,y)->y)
+
+        firstPlayerPile,secondPlayerPile
         
          
 
@@ -76,8 +90,7 @@ let main argv =
     let (first,second)=generateCards |> List.splitAt 26
     printfn "First player pile: %A" first
     printfn "Second player pile: %A" second
-    let firstPlayerPileAfterGame= formPointStack first second trump
-    let secondPlayerPileAfterGame= formPointStack second first trump
+    let (firstPlayerPileAfterGame,secondPlayerPileAfterGame)= formPointStack first second trump
     printfn "First player pile after the game: %A" firstPlayerPileAfterGame
     printfn "Second player pile after the game: %A" secondPlayerPileAfterGame
     printfn "Total score"
